f02738375cbf7b2d840f0f8cdf3e12133e574af2
sarviknathiyaa <81229718+sarviknathiyaa@users.noreply.github.com>
2021-09-08 11:49:45 +0530

    Add files via upload

---
+++ blue.py
@@ -0,0 +1,8 @@
+from tkinter import *
+
+"print="click me"
+root=Tk()
+dictionary["colour"][random_no]
+root.geometry("600x400")
+root.mainloop()
+"src"="https://upload.wikimedia.org/wikipedia/commons/7/7a/Bb_blue.jpg"
